cb136384614aa814ad05e0c61d81def0d1939391
Hkeoke <164091380+Hkeoke@users.noreply.github.com>
2024-12-19 02:42:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware  # Importa el middleware de CORS
 from pydantic import BaseModel
 import google.generativeai as genai
 
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],  # Permite todas las origins (puedes especificar dominios específicos)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos
+    allow_headers=["*"],  # Permite todos los headers
+)
+
 GOOGLE_API_KEY = "AIzaSyBNUvWgase8MvyckY8sronDMsWY1kzwLPc"
 genai.configure(api_key=GOOGLE_API_KEY)
 
